c0b7642304091a2ee9cf316ddaf5e7c2965a3f2d
jad-png <j.belassiria3993@uca.ac.ma>
2026-01-16 11:09:47 +0100

    chore: configure lazy-loaded routes for features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./features/library/library.component').then(m => m.LibraryComponent)
+    },
+    {
+        path: 'track/:id',
+        loadComponent: () => import('./features/track-detail/track-detail.component').then(m => m.TrackDetailComponent)
+    }
+];
