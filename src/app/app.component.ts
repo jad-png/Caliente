8c012a364af0f7a9d4c187e8a89ec1ad12bc0787
jad-png <j.belassiria3993@uca.ac.ma>
2026-01-16 11:09:47 +0100

    chore: update AppComponent imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PlayerControlsComponent } from './shared/components/player-controls/player-controls.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PlayerControlsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Calient17';
+  title = 'MusicStream';
 }
